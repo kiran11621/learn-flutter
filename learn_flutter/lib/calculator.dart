3498490c84184784a45d7365eb02a9b4a33e8370
Kiran Maharana <kiran11621@gmail.com>
2024-12-22 18:20:46 +0530

    calculator add button and its functionality added

--- learn_flutter/lib/calculator.dart
+++ learn_flutter/lib/calculator.dart
@@ -8,8 +8,100 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _num1Controller = TextEditingController();
+  final TextEditingController _num2Controller = TextEditingController();
+  final TextEditingController _answerController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+        backgroundColor: Colors.black54,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(
+          20,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _num1Controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter your Number 1",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _num2Controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter your Number 2",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    color: Colors.purpleAccent,
+                    hoverColor: Colors.grey,
+                    onPressed: () {
+                      _answerController.text =
+                          (double.parse(_num1Controller.text) +
+                                  double.parse(_num2Controller.text))
+                              .toString();
+                    },
+                    icon: const Icon(Icons.add),
+                  ),
+                  IconButton(
+                    color: Colors.purpleAccent,
+                    hoverColor: Colors.grey,
+                    onPressed: () {},
+                    icon: const Icon(Icons.remove),
+                  ),
+                  IconButton(
+                    color: Colors.purpleAccent,
+                    hoverColor: Colors.grey,
+                    onPressed: () {},
+                    icon: const Icon(Icons.close),
+                  ),
+                  IconButton(
+                    color: Colors.purpleAccent,
+                    hoverColor: Colors.grey,
+                    onPressed: () {},
+                    icon: const Icon(Icons.star),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _answerController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Answer",
+                ),
+                enabled: false,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
