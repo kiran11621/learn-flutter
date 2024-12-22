7b153cfa40daf4f54d7e46e4996feacfdb53feed
Kiran Maharana <kiran11621@gmail.com>
2024-12-22 17:35:46 +0530

    buttons and its type

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -16,7 +16,87 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Buttons(),
+    );
+  }
+}
+
+class Buttons extends StatefulWidget {
+  const Buttons({super.key});
+
+  @override
+  State<Buttons> createState() => _ButtonsState();
+}
+
+class _ButtonsState extends State<Buttons> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buttons and DialogBox'),
+        backgroundColor: Colors.black45,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    print('Printing On Press');
+                  },
+                  child: Text(
+                    'Button',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    print('Printing On Press');
+                  },
+                  child: Text(
+                    'Button',
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    print('Printing On Press');
+                  },
+                  child: Icon(Icons.download),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: () {
+                    print('Printing On Press');
+                  },
+                  label: Text('Upload'),
+                  icon: Icon(Icons.upload),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
